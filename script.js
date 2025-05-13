a7cb8c32a74d6ec13083c464cf0e43ef8b262c08
Israel <israelaguiar619@gmail.com>
2025-05-13 12:45:09 -0300

    Primeiro código da calculadora

---
+++ script.js
@@ -0,0 +1,29 @@
+const campo = document.getElementById("campo");
+
+const porcento = document.getElementById("porcento");
+const raiz = document.getElementById("raiz");
+const ce = document.getElementById("ce");
+const c = document.getElementById("c");
+
+const sete = document.getElementById("sete");
+const oito = document.getElementById("oito");
+const nove = document.getElementById("nove");
+const menos = document.getElementById("menos");
+
+const quatro = document.getElementById("quatro");
+const cinco = document.getElementById("cinco");
+const seis = document.getElementById("seis");
+const divisao = document.getElementById("divisao");
+
+const um = document.getElementById("um");
+const dois = document.getElementById("dois");
+const tres = document.getElementById("tres");
+const vezes = document.getElementById("vezes");
+
+const zero = document.getElementById("zero");
+const ponto = document.getElementById("ponto");
+const igual = document.getElementById("igual");
+const mais = document.getElementById("mais");
+
+
+
